b2c5ecdbce5e5e89bf118d0409feb466e70e06b7
Sarthak Dev <91304126+sarthakdev18@users.noreply.github.com>
2023-01-19 14:31:50 +0530

    Add files via upload

---
+++ LCMFinder.js
@@ -0,0 +1,12 @@
+const num1 = prompt('Enter your first positive integer: ');
+const num2 = prompt('Enter your second positive integer: ');
+
+let min = (num1 > num2) ? num1 : num2;
+
+while (true) {
+    if (min % num1 == 0 && min % num2 == 0) {
+        console.log(`The LCM of ${num1} and ${num2} is ${min}`);
+        break;
+    }
+    min++;
+}
